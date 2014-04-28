06a9b38570cbd4c97497d4b5f8919d784161c0fd
Mat Gargano <mgargano@gmail.com>
2014-04-28 09:54:06 -0400

    initial commit

---
+++ jetpack_development_mode.php
@@ -0,0 +1,11 @@
+<?php
+/*
+  Plugin Name: Jetpack Development Mode
+  Version: 0.0.1
+  Plugin URI: http://voceconnect.com/
+  Description: Turn on Jetpack Development Mode
+  Author: Voce Platforms
+  Author URI: http://voceconnect.com/
+ */
+
+add_filter( 'jetpack_development_mode', '__return_true' );
