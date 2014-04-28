86ea0185eb4841dd8ac5791c66133d671568c3a1
Mat Gargano <mgargano@gmail.com>
2014-04-28 11:09:40 -0400

    add check for defined (JETPACK_DEV_DEBUG) and if not define

--- jetpack-development-mode.php
+++ jetpack-development-mode.php
@@ -1,7 +1,7 @@
 <?php
 /*
   Plugin Name: Jetpack Development Mode
-  Version: 0.0.1
+  Version: 0.0.2
   Plugin URI: http://voceconnect.com/
   Description: Turn on Jetpack Development Mode
   Author: Voce Platforms
@@ -9,4 +9,6 @@
  */
 
 add_filter( 'jetpack_development_mode', '__return_true' );
-define( 'JETPACK_DEV_DEBUG', true );
+if ( ! defined( JETPACK_DEV_DEBUG ) ) {
+	define( 'JETPACK_DEV_DEBUG', true );
+}
